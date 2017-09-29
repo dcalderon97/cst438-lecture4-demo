d933ed18afc3d4faf0638ecf2c3309ac90c14918
Daniel Calderon <dcalderonmariavez@csumb.edu>
2017-09-29 02:48:22 +0000

    J

--- test/test.js
+++ test/test.js
@@ -66,3 +66,24 @@ describe("Getty api call", function() {
     
     
 }); 
+describe("GETTY", function() {
+    it("should return without errors", function(done) {
+        twitter.doAllTwitterRequests(function(error, tweets) {
+            //console.log("tweets: " + tweets.length); 
+            console.log("error: " + error); 
+            
+            expect(error).to.be.a('null'); 
+            done(); 
+        });
+    }); 
+    
+    it("should return a list of tweets", function(done) {
+        twitter.doAllTwitterRequests(function(error, tweets) {
+            //console.log("tweets: " + tweets.length); 
+            expect(tweets).to.be.a('array'); 
+            expect(tweets.length).to.be.above(0); 
+            done(); 
+        });
+    });
+    
+}); 
